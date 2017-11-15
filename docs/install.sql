5c80e175134d8920310c803d327533566476793a
Anton Fedurtsya <anton@fedurtsya.com>
2017-11-15 16:55:19 +0200

    OXDEV-339 Fix shop and admin id's

--- docs/install.sql
+++ docs/install.sql
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS `oeguestbookentry` (
 ) ENGINE=InnoDB COMMENT='Guestbook`s entries';
 
 INSERT INTO `oxseo` (`OXOBJECTID`, `OXIDENT`, `OXSHOPID`, `OXLANG`, `OXSTDURL`, `OXSEOURL`, `OXTYPE`, `OXFIXED`, `OXEXPIRED`, `OXPARAMS`) VALUES
-('d12b7badee1037e7c1a5a7a245a14e11', '7c8aa72aa16b7d4a859b22d8b8328412', 'oxbaseshop', 0, 'index.php?cl=oeguestbookguestbook', 'gaestebuch/', 'static', 0, 0, '')
+('d12b7badee1037e7c1a5a7a245a14e11', '7c8aa72aa16b7d4a859b22d8b8328412', 1, 0, 'index.php?cl=oeguestbookguestbook', 'gaestebuch/', 'static', 0, 0, '')
 ON DUPLICATE KEY UPDATE `OXSTDURL` = 'index.php?cl=oeguestbookguestbook';
 
 INSERT INTO `oxseo` (`OXOBJECTID`, `OXIDENT`, `OXSHOPID`, `OXLANG`, `OXSTDURL`, `OXSEOURL`, `OXTYPE`, `OXFIXED`, `OXEXPIRED`, `OXPARAMS`) VALUES
-('d12b7badee1037e7c1a5a7a245a14e11', 'ded4f3786c6f4d6d14e3034834ebdcaf', 'oxbaseshop', 1, 'index.php?cl=oeguestbookguestbook', 'en/guestbook/', 'static', 0, 0, '')
+('d12b7badee1037e7c1a5a7a245a14e11', 'ded4f3786c6f4d6d14e3034834ebdcaf', 1, 1, 'index.php?cl=oeguestbookguestbook', 'en/guestbook/', 'static', 0, 0, '')
 ON DUPLICATE KEY UPDATE `OXSTDURL` = 'index.php?cl=oeguestbookguestbook';
