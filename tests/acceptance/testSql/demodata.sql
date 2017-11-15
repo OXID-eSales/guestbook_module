79f20e50436c4513f8f6e305aa5a8b98971fcc63
Anton Fedurtsya <anton@fedurtsya.com>
2017-11-15 16:55:49 +0200

    OXDEV-339 Activate azure theme for tests

--- tests/acceptance/testSql/demodata.sql
+++ tests/acceptance/testSql/demodata.sql
@@ -1,3 +1,6 @@
+# Reset theme in config
+UPDATE `oxconfig` SET `OXVARVALUE` = encode('azure', 'fq45QS09_fqyx09239QQ') WHERE `OXVARNAME` = 'sTheme';
+
 #Users demodata
 REPLACE INTO `oxuser` (`OXID`,     `OXACTIVE`, `OXRIGHTS`, `OXSHOPID`,   `OXUSERNAME`,         `OXPASSWORD`,                       `OXPASSSALT`,        `OXCUSTNR`, `OXUSTID`, `OXCOMPANY`,          `OXFNAME`,        `OXLNAME`,           `OXSTREET`,        `OXSTREETNR`, `OXADDINFO`,                   `OXCITY`,            `OXCOUNTRYID`,                `OXZIP`, `OXFON`,        `OXFAX`,       `OXSAL`, `OXBONI`, `OXCREATE`,            `OXREGISTER`,          `OXPRIVFON`,   `OXMOBFON`,    `OXBIRTHDATE`) VALUES
                      ('testuser',  1,         'user',     1, 'example_test@oxid-esales.dev', 'c9dadd994241c9e5fa6469547009328a', '7573657275736572',   8,         '',        'UserCompany šÄßü?',  'UserNamešÄßü?',  'UserSurnamešÄßü?',  'Musterstr.šÄßü?', '1',          'User additional info šÄßü?',  'Musterstadt šÄßü?', 'a7c40f631fc920687.20179984', '79098',  '0800 111111', '0800 111112', 'Mr',     500,     '2008-02-05 14:42:42', '2008-02-05 14:42:42', '0800 111113', '0800 111114', '1980-01-01'),
